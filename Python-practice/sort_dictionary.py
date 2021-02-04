5f26db689357eb3fa2c53b3b79bbe45fb751cdd8
TheresaLin <lin40328@gmail.com>
2021-02-04 17:02:16 -0500

    add challenges for sorting dictionary

---
+++ Python-practice/sort_dictionary.py
@@ -0,0 +1,10 @@
+def sort_dict(dictionary):
+    # Please sort dictionary with the second value in list in ascending order
+    # {'key1': [..., a], 'key2': [..., b]}
+    # a needs to be smaller than b
+    # please finish this function
+    return dictionary
+
+if __name__ == '__main__':
+    d = {'k1': [2, 3], 'k2': [3, 2], 'k3': [5, 4], 'k4': [6, 1]}
+    print(sort_dict(d))
