e6abbaae8d5657b6d516d14101507ffab3583772
TheresaLin <lin40328@gmail.com>
2021-03-06 16:09:45 -0500

    upload Theresa's answer

--- Python-practice/two_cnt_sum.py
+++ Python-practice/two_cnt_sum.py
@@ -4,8 +4,22 @@ def two_cnt_sum(l, target):
     # e.g. [1, 2, 1, 2, 1]
     # then you should return [1, 2] because 1 shows up 3 times and 2 shows up 2 times
     # also you cannot return [2, 1]
+    
+        
     res = []
-    return res
+    
+    for i in l:
+        for j in l:
+            if i+j == target:
+                if i>j:
+                    res.append(j)
+                    res.append(i)
+                else:
+                    res.append(i)
+                    res.append(j)
+                    return res
+            else:
+                continue
 
 if __name__ == '__main__':
     ipt = [1, 2, 3, 2, 5, 1, 7, 2, 1, 9, 6, 3]
